be1ea3470004410eaba0cceee535ee58187c58c5
selianordev <40170929+selianordev@users.noreply.github.com>
2018-07-20 05:20:11 +0200

    Delete social-media.php

--- selemani-portfolio-single/includes/social-media.php
+++
@@ -1,9 +0,0 @@
-
-<div class="social">
-	<ul class="social list-inline">
-	    <li><a href="https://www.facebook.com/lusukamaselemani" target="_blank" title="Follow me on Facebook"><i class="fa fa-lg fa-facebook"></i></a></li>
-	    <li><a href="https://twitter.com/lusukamaselemani" target="_blank" title="Like me on Twitter"><i class="fa fa-lg fa-twitter"></i></a></li>
-	    <li><a href="" target="_blank" title="Talk to me on Google+" rel="publisher"><i class="fa fa-lg fa-google-plus"></i></a></li>
-	    <li><a href="https://za.linkedin.com/in/lusukama-selestin-selemani-5363a1136" target="_blank" title="Connect with me on Linkedin" ><i class="fa fa-lg fa-linkedin"></i></a></li>
-	</ul>
-</div>
